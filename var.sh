3d9c0fb70d1d7e2d93c866decd46cee5fc1768d6
shamim93 <shamim41.cse@gmail.com>
2025-01-28 05:05:01 -0500

    create a directory by taking user input

--- var.sh
+++ var.sh
@@ -4,6 +4,11 @@
 # greetings="Hello"
 # echo "$greetings $name"
 # user input
-echo "What is your name?"
-read uname
-echo "Hi $uname, How are things going on?"
+# echo "What is your name?"
+read -p "What is your name? " name #take user input from a single line, reduce code
+# read uname
+echo "Hi $name, How are things going on?"
+# take a user input and create a directory according to the input value
+
+read -p "Enter the directory name: " dirname
+mkdir $dirname
